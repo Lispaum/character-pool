3a70de1d76b5479d188c2dc32a1deaf307ed763f
lispaum <felippemda@gmail.com>
2023-02-19 21:50:25 -0300

    Beuatified generateLifePath page

--- src/pages/GameTools/index.tsx
+++ src/pages/GameTools/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { PageContainer } from '../styles'
-import { roll, testRNGRandomness } from './gameTools'
 import { GenerateLifePath } from './GenerateLifePath'
 
 type Race = 'Witcher' | 'Human' | 'Non-Human'
@@ -16,21 +15,36 @@ export function GameTools() {
     setLifePath(GenerateLifePath(characterRace))
   }
 
+  function handleRaceSelection(event: any) {
+    setCharacterRace(event.target.value)
+  }
+
   return (
     <PageContainer>
       <h1>Your Life Path</h1>
-      <h2 onClick={handleRerollLifePath}>Again</h2>
 
-      {Object.entries(lifePath).map(([key, value]) => {
-        return (
-          <div key={key}>
-            <h4>{key}</h4>
-            <p>{value}</p>
-          </div>
-        )
-      })}
+      <select onChange={handleRaceSelection} name="race" id="raceSelector">
+        <option value="Witcher">Witcher</option>
+        <option value="Human">Human</option>
+        <option value="Non-Human">Non-Human</option>
+      </select>
 
-      <h2>teste</h2>
+      <button type="button" onClick={handleRerollLifePath}>
+        Reroll Lifepath
+      </button>
+
+      <div id="lifePath">
+        {Object.entries(lifePath).map(([key, value]) => {
+          return (
+            <div key={key}>
+              <h4>{key}</h4>
+              <p>{value}</p>
+            </div>
+          )
+        })}
+      </div>
+
+      {/* <h2>teste</h2> */}
       {/* {testRNGRandomness().map((result, index) => {
         return <p key={index}>{result}</p>
       })} */}
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -5,4 +5,17 @@ export const PageContainer = styled.div`
   p {
     margin: 1rem 0;
   }
+
+  select,
+  button {
+    padding: 0.4rem;
+    border: none;
+    border-radius: 25px;
+
+    background-color: ${(props) => props.theme['fill-B'] ?? ''};
+    margin: 1rem 0.2rem;
+  }
+
+  div#lifePath {
+  }
 `
